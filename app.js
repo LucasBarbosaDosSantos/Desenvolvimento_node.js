208d57d1faa79ad16664af725de3887e0a3992ce
LucasBarbosaDosSantos <lucasantosb21@gmail.com.br>
2021-06-01 12:53:56 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+console.log("Hello Word!");
+console.log("Meu nome é Lucas!");
+console.log("E estou aprendendo Node,js com o guia do programador");
+
+if (mostrarSite) {//se mostrar site
+    console.log(site);//print o site
+}
